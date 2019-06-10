382eaeb0f0425a35392213a56b5ebf20634b2a08
小池貴之 <takayuki.koike@bebit.com>
2019-06-10 13:53:47 +0900

    feat: implement watch functionality

---
+++ emoji-status.js
@@ -0,0 +1,55 @@
+class EmojiStatus {
+  constructor(emojiName, channelId, timestamp) {
+    this.emojiName = emojiName;
+    this.channelId = channelId;
+    this.timestamp = timestamp;
+    this.histories = [];
+    this.isRemoved = false;
+  }
+
+  addHistory(userId, channelId, timestamp, permalink) {
+    this.histories.push({ userId, channelId, timestamp, permalink});
+  }
+
+  remove() {
+    this.isRemoved = true;
+  }
+
+  toBlocks() {
+    const blocks = [];
+
+    blocks.push({
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `:${this.emojiName}: ( \`:${this.emojiName}:\` ) が追加されました`
+      }
+    })
+
+    if (this.histories.length > 0) {
+      this.histories.forEach(h => {
+        blocks.push({
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `<@${h.userId}>が<#${h.channelId}>で使用しました\n${h.permalink}`
+          }
+        })
+      })
+    }
+
+    if (this.isRemoved) {
+      blocks.push({
+        "type": "section",
+        "text": {
+          "type": "plain_text",
+          "text": `このスタンプは削除されました`
+        }
+      })
+    }
+
+    return blocks;
+  }
+}
+
+module.exports = EmojiStatus;
--- index.js
+++ index.js
@@ -1,33 +1,72 @@
 const { WebClient } = require('@slack/web-api');
 const { createEventAdapter } = require('@slack/events-api');
+const EmojiStatus = require("./emoji-status");
 
 const token = process.env.SLACK_BOT_TOKEN;
 const channelId = process.env.CHANNEL_ID;
-const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const slackSigningSecret = process.env.SLACK_SIGNIN_SECRET;
+const port = process.env.PORT;
 
 const slackEvents = createEventAdapter(slackSigningSecret);
-const port = process.env.PORT || 3000;
 const web = new WebClient(token);
+
+const emojiStatuses = {};
+
+slackEvents.on('reaction_added', async (event) => {
+  const emojiName = event.reaction;
+
+  if (emojiStatuses[emojiName]) {
+    const status = emojiStatuses[emojiName];
+
+    const { permalink } = await web.chat.getPermalink({
+      channel: event.item.channel,
+      message_ts: event.item.ts
+    });
+
+    status.addHistory(event.user, event.item.channel, event.item.ts, permalink);
+
+    await web.chat.update({
+      channel: channelId,
+      ts: status.timestamp,
+      blocks: status.toBlocks()
+    });
+  }
+});
 
 slackEvents.on('emoji_changed', async (event) => {
   if (event.subtype === 'add') {
     const emojiName = event.name;
 
-
-    await web.chat.postMessage({
-      text: `:${emojiName}: ( \`:${emojiName}:\` ) が追加されました`,
+    const status = new EmojiStatus(emojiName, channelId);
+    const { ts } = await web.chat.postMessage({
+      blocks: status.toBlocks(),
       channel: channelId,
     });
+
+    status.timestamp = ts;
+    emojiStatuses[emojiName] = (status);
+
+    setTimeout(() => {
+      delete emojiStatuses[emojiName];
+    }, 300000);
   }
 
   if (event.subtype === 'remove') {
     const emojiNames = event.names;
 
     emojiNames.forEach(async (emojiName) => {
-      await web.chat.postMessage({
-        text: `:${emojiName}: ( \`:${emojiName}:\` ) が削除されました`,
-        channel: channelId,
-      });
+      if (emojiStatuses[emojiName]) {
+        const status = emojiStatuses[emojiName];
+        status.remove();
+
+        await web.chat.update({
+          channel: channelId,
+          ts: status.timestamp,
+          blocks: status.toBlocks()
+        });
+
+        delete emojiStatuses[emojiName];
+      }
     });
   }
 });
