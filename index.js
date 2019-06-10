2b11bba1b43c6e4ee8e56e411389a3eeb610951a
小池貴之 <takayuki.koike@bebit.com>
2019-06-10 09:51:19 +0900

    refactor: use web and events api instead of rtm-api

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-const { RTMClient } = require('@slack/rtm-api');
- 
-// Read a token from the environment variables
+const { WebClient } = require('@slack/web-api');
+const { createEventAdapter } = require('@slack/events-api');
+
 const token = process.env.SLACK_BOT_TOKEN;
 const channelId = process.env.CHANNEL_ID;
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 
- 
-// Initialize
-const rtm = new RTMClient(token);
+const slackEvents = createEventAdapter(slackSigningSecret);
+const port = process.env.PORT || 3000;
+const web = new WebClient(token);
 
-rtm.on('emoji_changed', (event) => {
+slackEvents.on('emoji_changed', async (event) => {
   if (event.subtype === 'add') {
     const emojiName = event.name;
 
-    rtm.sendMessage(`:${emojiName}: ( \`:${emojiName}:\` ) が追加されました`, channelId);
+
+    await web.chat.postMessage({
+      text: `:${emojiName}: ( \`:${emojiName}:\` ) が追加されました`,
+      channel: channelId,
+    });
   }
 
   if (event.subtype === 'remove') {
     const emojiNames = event.names;
 
-    emojiNames.forEach((emojiName) => {
-      rtm.sendMessage(`\`:${emojiName}:\` が削除されました`, channelId);
+    emojiNames.forEach(async (emojiName) => {
+      await web.chat.postMessage({
+        text: `:${emojiName}: ( \`:${emojiName}:\` ) が削除されました`,
+        channel: channelId,
+      });
     });
   }
 });
 
+slackEvents.on('error', console.error);
+
 (async () => {
-  // Connect to Slack
-  await rtm.start();
+  const server = await slackEvents.start(port);
+  console.log(`Listening for events on ${server.address().port}`);
 })();
